7b025b17267d45010853dfec9825f4cfd8925ec1
Arthur.c.s <130755987+arthur-canofre@users.noreply.github.com>
2024-08-15 09:34:24 -0300

    commit lista de tarefas

---
+++ projeto-app/app/lista-tarefa/Index.jsx
@@ -0,0 +1,66 @@
+import React, { useState, } from "react";
+import { StyleSheet, View, FlatList, Text, Button } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent:'space-between'
+    },
+    itemIn: {
+        textDecorationLine: 'none'
+    },
+    itemC: {
+        textDecorationLine: 'line-through'
+    },
+    titulo: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        paddingBottom: 30
+    }
+})
+
+const App = () => {
+
+    var [lista ,setLista] = useState([
+        {
+            id: 0,
+            tarefa: 'Consquistar o mundo',
+            completo: false
+        },
+        {
+            id: 1,
+            tarefa: 'Destruir o capitalismo',
+            completo: false
+        },
+        {
+            id: 2,
+            tarefa: 'Acabar com o machismo estrutual',
+            completo: false
+        },
+        {
+            id: 3,
+            tarefa: 'Tomar café da manhã',
+            completo: true
+        },
+        {
+            id: 4,
+            tarefa: 'Se juntar aos Iluminatti',
+            completo: false
+        }
+    ])
+
+    
+    return(
+    <View style={styles.container}>
+        <Text style={styles.titulo}>Lista de tarefas</Text>
+        <FlatList
+            data={lista}
+            renderItem={({item}) => <Text style={item.completo? styles.itemC: styles.itemIn}>{item.tarefa}</Text>} 
+            keyExtractor={item => item.id}
+        />
+    </View>
+    )
+}
+
+export default App
